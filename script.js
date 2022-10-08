8804f4fb58174ff457d237b86ef91a01ce7b2212
thecode-master-sy <114254116+thecode-master-sy@users.noreply.github.com>
2022-10-08 19:15:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger__icon');
 const mobileNavigation = document.querySelector('.navigation__mobile-container');
 const cancel = document.querySelector('.icon-cancel');
+const dropdownContainer = Array.from(document.querySelectorAll('.dropdown__container'));
+
+console.log(dropdownContainer)
 console.log(mobileNavigation);
 
 console.log(hamburger);
@@ -14,4 +17,15 @@ hamburger.addEventListener('click', ()=>{
 cancel.addEventListener('click', ()=>{
 	mobileNavigation.classList.remove('active')
 })
+
+dropdownContainer.forEach(drop=>{
+	drop.addEventListener('click', e=>{
+		if(drop.classList.contains('active')){
+			drop.classList.remove('active');
+		}else {
+			drop.classList.add('active')
+		}
+	})
+})
+
 
